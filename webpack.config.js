b61f439beadc084c2979d46298f8f85ac7906cf8
wh2887 <13567272790@163.com>
2020-06-24 00:15:53 +0800

    学到了自动创建html，自动更新引入文件以及title

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
-var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var path = require
+('path')
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js'
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: '自动测试',
+    template: 'src/assets/index.html'
+  })]
 }
